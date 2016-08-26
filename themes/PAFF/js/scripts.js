ceaa05cfa2b8786362434850ad6a401e19d086fe
Ana Timilic <atimilic@gmail.com>
2016-08-26 14:50:37 +0200

    slider top

--- themes/PAFF/js/scripts.js
+++ themes/PAFF/js/scripts.js
@@ -6,30 +6,43 @@
             $('.sponsors-slideshow').slick({
                 autoplay: true,
                 cssEase: 'linear',
+
                 infinite: true,
                 speed: 300,
                 variableWidth: true,
-                slidesToShow: 5,
+                slidesToShow: 3,
                 arrows: false,
                 centerMode: true
             });
         }
     };
 
-    $('.news-slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-    });
-    $('.news-slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
-    });
+    Drupal.behaviors.slider = {
+        attach: function(context, settings) {
+
+           $('.slider-slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 0,
+            arrows: false,
+            fade: false,
+            asNavFor: '.slider-slider-nav'
+          });-
+          $('.slider-slider-nav').slick({
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            asNavFor: '.slider-slider-for',
+            dots: false,
+            centerMode: true,
+            focusOnSelect: true,
+            infinite: true,
+            prevArrow: false,
+            nextArrow:false,
+            swipe: true,
+            slickPrev: false,
+            slickNext: false,
+
+          });
+        }
+    };
 
 }(jQuery, Drupal, drupalSettings));
