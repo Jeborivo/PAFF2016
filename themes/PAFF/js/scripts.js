1e3b451c6c475be07773232ee53ac9f8fb58b0fb
anarthul <anarthul@gmail.com>
2016-08-26 14:54:25 +0200

    Front page

--- themes/PAFF/js/scripts.js
+++ themes/PAFF/js/scripts.js
@@ -9,27 +9,12 @@
                 infinite: true,
                 speed: 300,
                 variableWidth: true,
-                slidesToShow: 5,
+                slidesToShow: 3,
                 arrows: false,
                 centerMode: true
             });
         }
     };
 
-    $('.news-slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-    });
-    $('.news-slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
-    });
 
 }(jQuery, Drupal, drupalSettings));
